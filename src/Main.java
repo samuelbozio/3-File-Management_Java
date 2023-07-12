4a036535039950067ff8514b7e2d6f41e8650283
Samuel Bozio <samuelbozio@hotmail.com>
2023-07-12 20:48:24 -0300

    mexendo com arquivos em java

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("D:\\texto.txt");
+
+        Scanner sc = null;
+        try {
+
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (sc != null)
+                sc.close();
+        }
+
+
+
+    }
+}
